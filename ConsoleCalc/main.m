a0ad2ddfca1941ed5d90864f8c457be946269316
Ильда  Залялов <IldarZalyalov@MacBook-Pro-Ilda.local>
2015-09-17 18:26:50 +0300

    init commit

---
+++ ConsoleCalc/main.m
@@ -0,0 +1,38 @@
+//
+//  main.m
+//  ConsoleCalc
+//
+//  Created by Ильда  Залялов on 17.09.15.
+//  Copyright (c) 2015 Ильда  Залялов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        float sum;
+        while(true){
+            NSLog (@"Read_ME: Данная программа использует стандартные  для вычислений операторы");
+            NSLog(@"Пример выражения: (2+2*2)*5-4*(2**2)/4");
+        NSLog(@"Введите выражение для вычисления БЕЗ пробелов: ");
+        char word[40];
+        
+        scanf("%39s", word);
+        @try{
+        
+        NSString *equation = [NSString stringWithCString:word encoding:1];;
+        NSExpression *expression = [NSExpression expressionWithFormat:equation];
+         sum = [[expression expressionValueWithObject:nil context:nil] floatValue];
+            
+        NSLog(@"Sum: %f \n", sum);
+            
+        }@catch(NSException *e){
+            NSLog(@"%@",e);
+            NSLog(@"Invalid arguments or else!");
+            NSLog(@"Pleas, check the letters!");
+            }
+        }
+        return 0;
+    }
+}
